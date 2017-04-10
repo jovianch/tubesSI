49f86b684b1c8491f63cabeff1787d6541dd01c3
Wiega Sonora <wiegasonora@gmail.com>
2017-04-10 21:48:39 +0700

    add login, modal after send messages

--- homePage.php
+++ homePage.php
@@ -37,8 +37,38 @@
 					  <tr>
 					    <td class="col-md-1 text-center">081281217726</td>
 					    <td class="col-md-1 text-center">Jovian</td>
-					    <td class="col-md-3">Cetak Spanduk 5 x 5 meter tapi bayarnya ngutang ke pak Idik soalnya dia gabawa dompet gitu trus duitnya jg udah abis buat bayar parkir mobil yang mahal</td>
+					    <td class="col-md-3">Cetak Spanduk 5 x 5 meter warna merah untuk event Wisuda HMIF</td>
 					    <td class="col-md-1 text-center">26/4/2017</td>
+					  </tr>
+					  <tr>
+					    <td class="col-md-1 text-center">083444555666</td>
+					    <td class="col-md-1 text-center">Rudy</td>
+					    <td class="col-md-3">Cetak Mug 500 item warna putih wisudaan HMIF</td>
+					    <td class="col-md-1 text-center">27/4/2017</td>
+					  </tr>
+					  <tr>
+					    <td class="col-md-1 text-center">089999999999</td>
+					    <td class="col-md-1 text-center">Susi</td>
+					    <td class="col-md-3">Cetak poster Kesenian Budaya Sunda 400 lembar</td>
+					    <td class="col-md-1 text-center">28/4/2017</td>
+					  </tr>
+					  <tr>
+					    <td class="col-md-1 text-center">085777777777</td>
+					    <td class="col-md-1 text-center">Fian</td>
+					    <td class="col-md-3">Cetak baliho 2x6 meter 5 item</td>
+					    <td class="col-md-1 text-center">29/4/2017</td>
+					  </tr>
+					  <tr>
+					    <td class="col-md-1 text-center">083555444111</td>
+					    <td class="col-md-1 text-center">Ari</td>
+					    <td class="col-md-3">Cetak Diktat Matematika 30 eksemplar @100 lembar</td>
+					    <td class="col-md-1 text-center">30/4/2017</td>
+					  </tr>
+					  <tr>
+					    <td class="col-md-1 text-center">083999000111</td>
+					    <td class="col-md-1 text-center">Chris</td>
+					    <td class="col-md-3">Cetak Poster Seminar Pendidikan 300 lembar</td>
+					    <td class="col-md-1 text-center">1/5/2017</td>
 					  </tr>
 					</table>
 				</div>
@@ -51,7 +81,7 @@
 							Create
 						</button>
 
-						<button id="btnEdit"type="button" class="btn btn-warning">
+						<button id="btnEdit"type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalSMS">
 							<i class="glyphicon glyphicon-pencil"></i>
 							Edit
 						</button>
@@ -61,12 +91,10 @@
 							Time
 						</button>
 
-					<a href="edit.html">
-						<button id="btnDelete" type="button" class="btn btn-danger">
+						<button id="btnDelete" type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalDelete">
 							<i class="glyphicon glyphicon-trash"></i>
 							Delete
 						</button>
-					</a>
 				</div>
 			</div>
 		</div>
@@ -79,6 +107,36 @@
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal">&times;</button>
 					<h4 class="modal-title">Create New</h4>
+				</div>
+				<form action="homePage.php" method="get" class="form-horizontal">
+					<div class="modal-body">
+							<div class="form-group">
+								<label for="noHP" class="col-sm-2 control-label">No HP</label>
+								<input type="text" id="noHP" name="noHP" placeholder="08XXXXXXXXXX" class="col-sm-9">
+						  </div>
+							<div class="form-group">
+								<label for="noHP" class="col-sm-2 control-label">Nama</label>
+								<input type="text" id="nama" name="nama" placeholder="Jovian Christianto" class="col-sm-9">
+							</div>
+							<div class="form-group">
+								<label for="noHP" class="col-sm-2 control-label">Pesanan</label>
+								<textarea type="text" id="pesanan" name="pesanan" placeholder="Cetak spanduk 5x10m" rows="5" class="col-sm-9"></textarea>
+							</div>
+						</div>
+						<div class="modal-footer">
+							<button type="submit" class="btn btn-success">Save</button>
+						</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<!-- MODAL EDIT -->
+	<div class="modal fade" id="modalEdit" role="dialog">
+		<div class="modal-dialog modal-sm">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">Edit</h4>
 				</div>
 				<form action="/homePage.php" method="get" class="form-horizontal">
 					<div class="modal-body">
@@ -130,7 +188,62 @@
 			</div>
 		</div>
 	</div>
-
+	<!-- MODAL DELETE -->
+	<div class="modal fade" id="modalDelete" role="dialog">
+		<div class="modal-dialog modal-sm">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">Delete</h4>
+				</div>
+				<form action="/homePage.php" method="get" class="form-horizontal">
+					<div class="modal-body">
+							<div class="form-group">
+								<label for="noHP" class="col-sm-2 control-label">No HP</label>
+								<input type="text" id="noHP" name="noHP" placeholder="08XXXXXXXXXX" class="col-sm-9">
+							</div>
+							<div class="form-group">
+								<label for="noHP" class="col-sm-2 control-label">Nama</label>
+								<input type="text" id="nama" name="nama" placeholder="Jovian Christianto" class="col-sm-9">
+							</div>
+							<div class="form-group">
+								<label for="noHP" class="col-sm-2 control-label">Pesanan</label>
+								<textarea type="text" id="pesanan" name="pesanan" placeholder="Cetak spanduk 5x10m" rows="5" class="col-sm-9"></textarea>
+							</div>
+						</div>
+						<div class="modal-footer">
+							<button type="submit" class="btn btn-danger">Delete</button>
+						</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<!-- MODAL PENGIRIMAN SMS -->
+	<div class="modal fade" id="modalSMS" role="dialog">
+		<div class="modal-dialog modal-sm">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">Pengiriman SMS</h4>
+				</div>
+				<form action="homePage.php" method="get" class="form-horizontal">
+					<div class="modal-body">
+							<div class="form-group">
+								<label for="kepada" class="col-sm-2 control-label">Kepada</label>
+								<input type="text" id="kepada" name="kepada" placeholder="" class="col-sm-9">
+							</div>
+							<div class="form-group">
+								<label for="noHP" class="col-sm-2 control-label">Konten</label>
+								<textarea type="text" id="pesanan" name="pesanan" placeholder="" rows="5" class="col-sm-9"></textarea>
+							</div>
+						</div>
+						<div class="modal-footer">
+							<button type="submit" class="btn btn-success">Done</button>
+						</div>
+				</form>
+			</div>
+		</div>
+	</div>
 
 	</body>
 </html>
---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<head>
+		<title>Aplikasi Pengingat Pengambilan Produk</title>
+	</head>
+	<body>
+		<div id="bodii2">
+			<div id="bodii">
+				<div id="judul">
+					<h1 class="col-md-10 col-md-offset-1">Aplikasi Pengingat Pengambilan Produk</h1>
+				</div>
+
+				<div id="pencarian">
+
+				</div>
+
+				<div id="login-panel">
+					<form action="homePage.php" method="post" class="form-group">
+					<div class="panel panel-primary">
+				  	<div class="panel-heading">Silakan Login ke Sistem</div>
+				    <div class="panel-body">
+										<div class="form-inline">
+											<label for="username" class="col-sm-3 control-label">Username</label>
+											<input type="text" id="username" name="username" placeholder="example" class="col-sm-4">
+									  </div>
+										<br>
+										<br>
+										<div class="form-group">
+											<label for="password" class="col-sm-3 control-label">Password</label>
+											<input type="password" id="password" name="password" placeholder="xxxxxxxxx" class="col-sm-4">
+										</div>
+						</div>
+						<div class="panel-footer">
+								<button type="submit" class="btn btn-success" id="login-button">Login</button>
+						</div>
+				  </div>
+					</form>
+				</div>
+	</body>
+</html>
